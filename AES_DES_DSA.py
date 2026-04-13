956894f9149c4fc3757d3f724805179e00bb1d81
Kirtanpatel11 <120971951+Kirtanpatel11@users.noreply.github.com>
2026-04-13 23:45:31 +0530

    Removed UI
    Removed UI and just little bit changes in project task is just added Blockchain based Text encryption which was not mentioned in Task sheet.

--- AES_DES_DSA.py
+++ AES_DES_DSA.py
@@ -6,32 +6,32 @@ import hashlib
 import time
 
 
-# class Block:
-#     def __init__(self, index, data, previous_hash):
-#         self.index = index
-#         self.timestamp = time.time()
-#         self.data = data
-#         self.previous_hash = previous_hash
-#         self.hash = self.calculate_hash()
+class Block:
+    def __init__(self, index, data, previous_hash):
+        self.index = index
+        self.timestamp = time.time()
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.calculate_hash()
 
-#     def calculate_hash(self):
-#         blockString = str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash)
-#         return hashlib.sha256(blockString.encode()).hexdigest()
+    def calculate_hash(self):
+        blockString = str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash)
+        return hashlib.sha256(blockString.encode()).hexdigest()
     
-# class Blockchain:
-#     def __init__(self):   
-#         self.chain = [self.create_genesis_block()]
+class Blockchain:
+    def __init__(self):   
+        self.chain = [self.create_genesis_block()]
 
-#     def create_genesis_block(self):
-#         return Block(0, "Genesis Block", "0")
+    def create_genesis_block(self):
+        return Block(0, "Genesis Block", "0")
     
-#     def get_latest_block(self):
-#         return self.chain[-1]
+    def get_latest_block(self):
+        return self.chain[-1]
     
-#     def add_block(self, data):  
-#         latest_block = self.get_latest_block()
-#         new_block = Block(len(self.chain), data, latest_block.hash)
-#         self.chain.append(new_block)
+    def add_block(self, data):  
+        latest_block = self.get_latest_block()
+        new_block = Block(len(self.chain), data, latest_block.hash)
+        self.chain.append(new_block)
 
 def AESpad(details):
     return details + (16 - len(details) % 16) * ' '
@@ -70,12 +70,12 @@ if choice == '1':
     print("Key:", key)
     print("")
 
-    # bc = Blockchain()
-    # bc.add_block(details)
-    # print("\n --Blockchain Mode--")
+    bc = Blockchain()
+    bc.add_block(details)
+    print("\n --Blockchain Mode--")
 
-    # for block in bc.chain:
-    #     print(f"Index: {block.index}, Data: {block.data}, Hash: {block.hash}, previous Hash: {block.previous_hash}")
+    for block in bc.chain:
+        print(f"Index: {block.index}, Data: {block.data}, Hash: {block.hash}, previous Hash: {block.previous_hash}")
 
 elif choice == '2':
     key = b'8bytekey'
@@ -85,12 +85,12 @@ elif choice == '2':
     print("Key:", key)
     print("")
 
-    # bc = Blockchain()
-    # bc.add_block(details)
-    # print("\n --Blockchain Mode--")
+    bc = Blockchain()
+    bc.add_block(details)
+    print("\n --Blockchain Mode--")
 
-    # for block in bc.chain:
-    #     print(f"Index: {block.index}, Data: {block.data}, Hash: {block.hash}, previous Hash: {block.previous_hash}")
+    for block in bc.chain:
+        print(f"Index: {block.index}, Data: {block.data}, Hash: {block.hash}, previous Hash: {block.previous_hash}")
 
 elif choice == "3":
     key_pair = RSA.generate(2048)
@@ -104,12 +104,12 @@ elif choice == "3":
     print("\nPrivate Key:", key_pair.export_key().decode())
     print("")
 
-    # bc = Blockchain()
-    # bc.add_block(details)
-    # print("\n --Blockchain Mode--")
+    bc = Blockchain()
+    bc.add_block(details)
+    print("\n --Blockchain Mode--")
 
-    # for block in bc.chain:
-    #     print(f"Index: {block.index}, Data: {block.data}, Hash: {block.hash}, previous Hash: {block.previous_hash}")
+    for block in bc.chain:
+        print(f"Index: {block.index}, Data: {block.data}, Hash: {block.hash}, previous Hash: {block.previous_hash}")
     
 
 else: 
